2469dc86efe86b94d9e21601924758928403b040
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-12-19 17:11:48 -0300

    main: feat (GameStore) - Added UpdateGame DTO and updated games API
    - Introduced an UpdateGame DTO and updated the games API implementation and sample HTTP requests to support and demonstrate update operations.
    - Adjusted the in-memory data and endpoint behavior to align with the new DTO and ensure the update flow works as expected.

--- src/GameStore/Program.cs
+++ src/GameStore/Program.cs
@@ -37,8 +37,15 @@ app.MapGet("/ping", () => "Pong!");
 app.MapGet("games", () => games);
 
 // GET /games/1
-app.MapGet("games/{id}", (int id) => games.Find(games => games.Id == id))
-    .WithName(GetGameEndpointName);
+app.MapGet("games/{id}", (int id) =>
+{
+    var game = games.Find(g => g.Id == id);
+
+    return game is null
+        ? Results.NotFound()
+        : Results.Ok(game);
+})
+.WithName(GetGameEndpointName);
 
 // POST /games
 app.MapPost("games", (CreateGameDto newGame) =>
@@ -54,6 +61,35 @@ app.MapPost("games", (CreateGameDto newGame) =>
     games.Add(game);
 
     return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
+
+// PUT /games
+app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
+
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate
+    );
+
+    return Results.NoContent();
+});
+
+// DELETE /games/1
+app.MapDelete("games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id == id);
+
+    return Results.NoContent();
 });
 
 app.Run();
