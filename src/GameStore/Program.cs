443aadb42966ad2584f9de195db1a443cef63a2b
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-12-19 16:39:15 -0300

    main: feat (GameStore) - Added game DTOs and initial API endpoints
    - Introduced Game and CreateGame DTOs and populated an in-memory games collection with basic GET and POST endpoints to expose the initial GameStore API surface.
    - Added an HTTP request example for manual testing and updated launch settings for local development.

--- src/GameStore/Program.cs
+++ src/GameStore/Program.cs
@@ -1,7 +1,59 @@
+using GameStore.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+const string GetGameEndpointName = "GetName";
+
+List<GameDto> games = [
+    new (
+        1,
+        "The Witcher 3",
+        "RPG",
+        99.90m,
+        new DateOnly(2015, 5, 19)
+    ),
+    new (
+        2,
+        "Hollow Knight",
+        "Metroidvania",
+        46.99m,
+        new DateOnly(2017, 2, 24)
+    ),
+    new (
+        3,
+        "Stardew Valley",
+        "Simulation",
+        24.99m,
+        new DateOnly(2016, 2, 26)
+    )
+];
+
+// GET /ping
+app.MapGet("/ping", () => "Pong!");
+
+// GET /games
+app.MapGet("games", () => games);
+
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(games => games.Id == id))
+    .WithName(GetGameEndpointName);
+
+// POST /games
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
 
 app.Run();
