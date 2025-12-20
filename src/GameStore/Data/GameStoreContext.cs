d9e9d8677e118d31fd2ab9104b333ae3d282cfe8
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-12-20 01:23:06 -0300

    main: feat (GameStore) - Added SeedGenres migration and seed genres in DbContext
    - Added a migration that seeds initial genre data and updated the DbContext OnModelCreating to include the seeded genres; the model snapshot was updated accordingly.

--- src/GameStore/Data/GameStoreContext.cs
+++ src/GameStore/Data/GameStoreContext.cs
@@ -13,4 +13,23 @@ public class GameStoreContext : DbContext
     public DbSet<Game> Games => Set<Game>();
 
     public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Fighting" },
+            new { Id = 2, Name = "Action" },
+            new { Id = 3, Name = "Adventure" },
+            new { Id = 4, Name = "RPG" },
+            new { Id = 5, Name = "Shooter" },
+            new { Id = 6, Name = "Sports" },
+            new { Id = 7, Name = "Racing" },
+            new { Id = 8, Name = "Strategy" },
+            new { Id = 9, Name = "Simulation" },
+            new { Id = 10, Name = "Puzzle" },
+            new { Id = 11, Name = "Horror" },
+            new { Id = 12, Name = "Platformer" },
+            new { Id = 13, Name = "MMO" }
+        );
+    }
 }
